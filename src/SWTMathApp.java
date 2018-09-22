030711dc29dc365edde642667839b8c5e9db9eb7
Greg5519 <gregory.nestor@peelsb.com>
2018-09-22 15:29:14 -0400

    Spinner Added

--- src/SWTMathApp.java
+++ src/SWTMathApp.java
@@ -1,5 +1,9 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Spinner;
 
 public class SWTMathApp {
 
@@ -8,7 +12,28 @@ public class SWTMathApp {
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
-		shell.setText("Hello Greg!");
+		shell.setText("4C SWT Math Assignment");
+		
+	    Spinner spinner = new Spinner(shell, SWT.BORDER);
+	    spinner.setToolTipText("Gregs Spinner");
+	    spinner.setMinimum(0);
+	    spinner.setMaximum(20);
+	    spinner.setSelection(10);
+	    spinner.setIncrement(1);
+	    spinner.setPageIncrement(5);
+	    
+	    
+		spinner.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				Integer value = spinner.getSelection();
+				spinner.setToolTipText("Value is: " + value.toString());
+			}
+		});
+		
+	    spinner.pack();
+
+	    //shell.pack();
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) display.sleep();
